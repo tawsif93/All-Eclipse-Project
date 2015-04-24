8d7b1735b442ad1319c3324ee801c5fd66fa0608
Home <Home@Home-PC>
2015-04-24 18:06:16 +0600

    Producer Consumer Added

---
+++ src/deitel/exercise/chapter26/Consumer.java
@@ -0,0 +1,39 @@
+package deitel.exercise.chapter26;
+
+import java.util.Random;
+
+public class Consumer implements Runnable
+{
+
+	private final Buffer SharedLocation ;
+	private final Random genarator = new Random() ;
+
+	public Consumer(Buffer shared ) {
+		// TODO Auto-generated constructor stub
+		SharedLocation = shared ;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		int sum = 0 ;
+
+		for(int count = 1 ; count <= 10 ; count++ )
+		{
+			try {
+				Thread.sleep(genarator.nextInt(3000));
+				int value = SharedLocation.get();
+				sum+= value ;
+				System.out.printf("\t\t\t%2d\n" , sum );
+
+
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("\nConsumer Reads value Totaling " + sum + "\nTerminating Consumer");;
+	}
+
+}
---
+++ src/deitel/exercise/chapter26/Producer.java
@@ -0,0 +1,38 @@
+package deitel.exercise.chapter26;
+
+import java.util.Random;
+
+public class Producer implements Runnable{
+
+	private final Buffer SharedLocation  ;
+	private final Random genarator = new Random();
+	
+
+	public Producer(Buffer shared) {
+		// TODO Auto-generated constructor stub
+		SharedLocation = shared ;
+	}
+	@Override
+	public void run(){
+		// TODO Auto-generated method stub
+		int sum = 0 ;
+		
+		for (int count = 1 ; count <= 10 ; count++)
+		{
+			try {
+				Thread.sleep(genarator.nextInt(3000));
+				SharedLocation.set(count);
+				sum += count ;
+				System.out.printf("\t%2d\n" , sum);
+			
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		System.out.println("producer done producing\nTerminating Producer.");
+	}
+	
+}
