30659eef53ed5a0d9a70f91992a9b37358312901
tawsif <tawsif93@gmail.com>
2015-06-03 17:23:46 +0600

    Practice pushed

--- src/deitel/exercise/chapter22/List.java
+++ src/deitel/exercise/chapter22/List.java
@@ -1,117 +1,200 @@
-package deitel.exercise.chapter22;
+package deitel.exercise.chapter22 ;
 
-public class List <T extends Comparable<T>>{
+public class List<T extends Comparable< T>> {
 
-	private ListNode<T> lastNode ;
-	private ListNode<T> firstNode ;
-	private String name ;
+	private ListNode< T>	lastNode ;
+	private ListNode< T>	firstNode ;
+	private String			name ;
 
-	public List() {
+	public List ( ) {
 		// TODO Auto-generated constructor stub
-		this("list");
+		this("list") ;
 
 	}
 
-	public List(String string) {
+	public List ( String string ) {
 		// TODO Auto-generated constructor stub
 		name = string ;
 		firstNode = lastNode = null ;
 	}
 
-	public void insertAtTheFront(T insertItem) {
-		if(isEmpty())
-			firstNode= lastNode = new ListNode<T>(insertItem);
-		else 
-			firstNode = new ListNode<T>(insertItem, firstNode);
+	public void insertAtTheFront( T insertItem ) {
+		if (isEmpty())
+			firstNode = lastNode = new ListNode< T>(insertItem) ;
+		else
+			firstNode = new ListNode< T>(insertItem, firstNode) ;
 	}
 
-	public void insertAtTheBack( T insertItem )
-	{
-		if(isEmpty()) 
-			firstNode = lastNode = new ListNode<T>(insertItem);
-		else 
-			lastNode = lastNode.nextNode = new ListNode<T>(insertItem	);
+	public void insertAtTheBack( T insertItem ) {
+		if (isEmpty())
+			firstNode = lastNode = new ListNode< T>(insertItem) ;
+		else
+			lastNode = lastNode.nextNode = new ListNode< T>(insertItem) ;
 	}
 
-	public void insertOrder(T element) {
-		
-		
-		if(firstNode == null )
+	/*
+	 * This Insert at the Back without tail reference added for exercise 22.27
+	 */
+	public void insertAtTheBackWithoutTail( T insertItem ) {
+
+		ListNode< T> currentNode = firstNode ;
+
+		if (firstNode == null)
 		{
-			firstNode= lastNode = new ListNode<T>(element);
+			firstNode = new ListNode< T>(insertItem) ;
 			return ;
 		}
-		
-		else if(element.compareTo(firstNode.data)< 0)
+
+		while (currentNode.nextNode != null)
+			currentNode = currentNode.nextNode ;
+
+		currentNode.nextNode = new ListNode< T>(insertItem) ;
+	}
+
+	public void insertOrder( T element ) {
+
+		if (firstNode == null)
 		{
-			insertAtTheFront(element);
+			firstNode = lastNode = new ListNode< T>(element) ;
 			return ;
 		}
-		else if (element.compareTo(lastNode.data) >= 0 )
+
+		else if (element.compareTo(firstNode.data) < 0)
 		{
-			insertAtTheBack(element);
-			return;
+			insertAtTheFront(element) ;
+			return ;
 		}
-		
-		ListNode<T> current = firstNode; 
-		
-		while(current != lastNode )
+		else if (element.compareTo(lastNode.data) >= 0)
 		{
-			if(element.compareTo(current.getNextNode().data) <= 0 )
+			insertAtTheBack(element) ;
+			return ;
+		}
+
+		ListNode< T> current = firstNode ;
+
+		while (current != lastNode)
+			if (element.compareTo(current.getNextNode().data) <= 0)
 			{
-				current.nextNode = new ListNode<T>(element, current.getNextNode());
+				current.nextNode = new ListNode< T>(element,
+						current.getNextNode()) ;
 				return ;
 			}
-			else 
-				current = current.getNextNode();
+			else
+				current = current.getNextNode() ;
+	}
+
+	/*
+	 * This method Insert at any position added for exercise 22.26
+	 */
+	public void insertAtAnyPosition( T value, T afterValue ) {
+		ListNode< T> currentNode = firstNode ;
+		boolean insertAtTheBack = true ;
+
+		while (true)
+			if (firstNode.data.compareTo(afterValue) > 0)
+			{
+				insertAtTheBack = false ;
+				insertAtTheFront(value) ;
+				break ;
+			}
+			else if (currentNode.nextNode == null)
+				break ;
+			else if ((currentNode.data.compareTo(afterValue) <= 0)
+					&& (currentNode.nextNode.data.compareTo(afterValue) > 0))
+			{
+				currentNode.nextNode = new ListNode< T>(value,
+						currentNode.nextNode) ;
+				insertAtTheBack = false ;
+				break ;
+			}
+
+			else
+				currentNode = currentNode.nextNode ;
+		if (insertAtTheBack == true)
+			insertAtTheBack(value) ;
+	}
+
+	/*
+	 * This method Delete at any position added for exercise 22.26
+	 */
+
+	public void deleteAtAnyPosition( T value ) {
+		ListNode< T> currentNode = firstNode ;
+		ListNode< T> old = firstNode ;
+
+		if (currentNode == null)
+			System.out.println("List is Empty") ;
+
+		while (true)
+		{
+			if (currentNode.data.equals(value))
+			{
+				if (firstNode.equals(currentNode))
+				{
+					firstNode = currentNode.nextNode ;
+					currentNode.nextNode = null ;
+					currentNode = firstNode ;
+				}
+				else
+				{
+					old.nextNode = currentNode.nextNode ;
+					currentNode.nextNode = null ;
+					currentNode = old ;
+				}
+				break ;
+			}
+			if (currentNode.nextNode == null)
+			{
+				System.out.println("No Roll Found") ;
+				break ;
+			}
+			else
+			{
+				old = currentNode ;
+				currentNode = currentNode.nextNode ;
+			}
 		}
 	}
-	
-	public void mergeSortList(List<T> secondList )
-	{
+
+	public void mergeSortList( List< T> secondList ) {
 		try
 		{
-			while(true)
-			{
-				this.insertOrder(secondList.removeFromFront());
-			}
-		}catch (EmptyListException exception)
+			while (true)
+				this.insertOrder(secondList.removeFromFront()) ;
+		} catch ( EmptyListException exception )
 		{
-			return  ;
+			return ;
 		}
 	}
-	
-	public T removeFromFront() throws EmptyListException
-	{
-		if(isEmpty())
-			throw new EmptyListException(name );
+
+	public T removeFromFront() throws EmptyListException {
+		if (isEmpty())
+			throw new EmptyListException(name) ;
 
 		T removeData = firstNode.data ;
 
-		if(lastNode == firstNode)
+		if (lastNode == firstNode)
 			lastNode = firstNode = null ;
 
-		else 
+		else
 			firstNode = firstNode.nextNode ;
 
 		return removeData ;
 	}
 
-	public T removeFromBack() throws EmptyListException
-	{
+	public T removeFromBack() throws EmptyListException {
 		T removeData = lastNode.data ;
 
-		if(isEmpty()) {
-			throw new EmptyListException();
-		}
+		if (isEmpty())
+			throw new EmptyListException() ;
 
-		if(firstNode == lastNode) 
+		if (firstNode == lastNode)
 			firstNode = lastNode = null ;
-		else 
+		else
 		{
-			ListNode<T> currentNode = firstNode ;
+			ListNode< T> currentNode = firstNode ;
 
-			while(currentNode.nextNode != lastNode)
+			while (currentNode.nextNode != lastNode)
 				currentNode = currentNode.nextNode ;
 
 			lastNode = currentNode ;
@@ -121,54 +204,81 @@ public class List <T extends Comparable<T>>{
 		return removeData ;
 	}
 
-	public void print()
-	{
-		if(isEmpty())
+	/*
+	 * added for Exercise 22.20
+	 */
+	public void printListBackward() {
+		reverse(firstNode) ;
+	}
+
+	private void reverse( ListNode< T> node ) {
+		if (node != null)
 		{
-			System.out.println("Empty " + name );
+			reverse(node.nextNode) ;
+			System.out.print(node.data + " ") ;
+		}
+	}
+
+	public void print() {
+		if (isEmpty())
+		{
+			System.out.println("Empty " + name) ;
 			return ;
 		}
 
-		System.out.printf( "The %s is: ", name );
+		System.out.printf("The %s is: ", name) ;
 
-		ListNode<T> current = firstNode ;
+		ListNode< T> current = firstNode ;
 
-		while(current != null)
+		while (current != null)
 		{
-			System.out.print(current.data + " " );
+			System.out.print(current.data + " ") ;
 			current = current.nextNode ;
 		}
-		System.out.println();
+		System.out.println() ;
+	}
+
+	/*
+	 * Search List added for Exercise 22.21
+	 */
+	public T searchList( T value ) {
+		ListNode< T> currentNode = firstNode ;
+
+		while (currentNode != null)
+			if (currentNode.data.equals(value))
+				return currentNode.data ;
+			else
+				currentNode = currentNode.nextNode ;
+
+		return null ;
 	}
 
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		if(firstNode == null) return true  ;
+		if (firstNode == null)
+			return true ;
 
-		return false;
+		return false ;
 	}
 }
 
-class ListNode <T extends Comparable<T>>{
+class ListNode<T extends Comparable< T>> {
 
-	T data ;
+	T				data ;
 
-	ListNode<T> nextNode ;
+	ListNode< T>	nextNode ;
 
-	public ListNode(T data, ListNode<T> node) {
-		this.data = data;
-		this.nextNode = node;
+	public ListNode ( T data , ListNode< T> node ) {
+		this.data = data ;
+		this.nextNode = node ;
 	}
 
-
-	public ListNode(T data )
-	{
-		this(data , null);
+	public ListNode ( T data ) {
+		this(data, null) ;
 	}
 
-
-	public ListNode<T> getNextNode() {
-		return nextNode;
+	public ListNode< T> getNextNode() {
+		return nextNode ;
 	}
 
 }
--- src/deitel/exercise/chapter22/Tree.java
+++ src/deitel/exercise/chapter22/Tree.java
@@ -1,101 +1,196 @@
 /**
  * 
  */
-package deitel.exercise.chapter22;
+package deitel.exercise.chapter22 ;
+
+import java.util.LinkedList ;
+import java.util.Queue ;
 
 /**
- * @author mohammedtawsif
- * May 12, 2015 8:05:49 PM
+ * @author mohammedtawsif May 12, 2015 8:05:49 PM
  * 
  */
-public class Tree <T extends Comparable<T>>{
+public class Tree<T extends Comparable< T>> {
 
-	private TreeNode<T> root ;
+	private TreeNode< T>	root ;
 
-	public Tree() {
+	private int				currentDepth ;
+	private int				depth ;
+
+	public Tree ( ) {
 		// TODO Auto-generated constructor stub
 		root = null ;
+		currentDepth = 0 ;
+		depth = 0 ;
 	}
 
 	/**
 	 * @param node
 	 */
-	public void insertNode(T node) {
+	public void insertNode( T node ) {
 
-		if(root == null )
-			root = new TreeNode<T>(node);
-		else 
-			root.insert(node);
-		
+		if (root == null)
+			root = new TreeNode< T>(node) ;
+		else
+			root.insert(node) ;
 	}
-	
-	
-	public void preorderTraversel()
-	{
-		preorderHelper(root);
+
+	public void preorderTraversel() {
+		preorderHelper(root) ;
 	}
 
 	/**
 	 * @param root
 	 */
-	private void preorderHelper(TreeNode<T> node) {
-		// TODO Auto-generated method stub
-		if ( node == null )
-			return;
-		
-		System.out.printf( "%s ", node.data );
-		preorderHelper( node.leftChild ); 
-		preorderHelper( node.rightChild );
+
+	public int getDepth() {
+		return depth(root) ;
 	}
-	
-	public void inorderTraversel()
-	{
-		inorderHelper(root);
+
+	private int depth( TreeNode< T> n ) {
+		if (n != null)
+		{
+			currentDepth++ ;
+
+			// record total depth if current depth is greater than total depth
+			if (currentDepth > depth)
+				depth = currentDepth ;
+
+			// recursively traverse entire tree
+			depth(n.leftChild) ;
+			depth(n.rightChild) ;
+
+			// decrement as we traverse up the binary tree
+			currentDepth-- ;
+		}
+		return depth ;
+	}
+
+	public void inorderTraversel() {
+		inorderHelper(root) ;
 	}
 
 	/**
 	 * @param node
 	 */
-	private void inorderHelper(TreeNode<T> node) {
+	private void inorderHelper( TreeNode< T> node ) {
 		// TODO Auto-generated method stub
-		
-		if ( node == null )
-			return;
-		
-		
-		inorderHelper( node.leftChild ); 
-		System.out.printf( "%s ", node.data );
-		inorderHelper( node.rightChild );
+
+		if (node == null)
+			return ;
+
+		inorderHelper(node.leftChild) ;
+		System.out.printf("%s ", node.data) ;
+		inorderHelper(node.rightChild) ;
 	}
-	
-	public void postorderTraversel()
-	{
-		postorderHelper(root);
+
+	public void postorderTraversel() {
+		postorderHelper(root) ;
+	}
+
+	private void preorderHelper( TreeNode< T> node ) {
+		// TODO Auto-generated method stub
+		if (node == null)
+			return ;
+
+		System.out.printf("%s ", node.data) ;
+		preorderHelper(node.leftChild) ;
+		preorderHelper(node.rightChild) ;
 	}
 
 	/**
 	 * @param node
 	 */
-	private void postorderHelper(TreeNode<T> node) {
+	private void postorderHelper( TreeNode< T> node ) {
 		// TODO Auto-generated method stub
-		if ( node == null )
-			return;
-		
-		postorderHelper( node.leftChild ); 
-		postorderHelper( node.rightChild );
-		System.out.printf( "%s ", node.data );
+		if (node == null)
+			return ;
+
+		postorderHelper(node.leftChild) ;
+		postorderHelper(node.rightChild) ;
+		System.out.printf("%s ", node.data) ;
 	}
-	
+
+	/*
+	 * This Method added for exercise 22.24
+	 */
+	public void levelOrderTraversal() {
+		Queue< TreeNode< T>> queue = new LinkedList<>() ;
+
+		queue.add(root) ;
+
+		while (!queue.isEmpty())
+		{
+			TreeNode< T> currentNode = queue.poll() ;
+			System.out.print(currentNode.data + " ") ;
+			if (currentNode.leftChild != null)
+				queue.add(currentNode.leftChild) ;
+			if (currentNode.rightChild != null)
+				queue.add(currentNode.rightChild) ;
+
+		}
+	}
+
+	/*
+	 * OutputTree Method added for exercise 22.25
+	 */
+
+	public void outputTree() {
+		outputTreeHelper(0, root) ;
+	}
+
+	private void outputTreeHelper( int totalSpace, TreeNode< T> current ) {
+		// TODO Auto-generated method stub
+		while (current != null)
+		{
+			outputTreeHelper(totalSpace + 5, current.rightChild) ;
+
+			for (int i = 0; i < totalSpace; i++)
+				System.out.print(" ") ;
+
+			System.out.println(current.data) ;
+
+			current = current.leftChild ;
+
+			totalSpace += 5 ;
+		}
+	}
+
+	/*
+	 * Method FindKey & Method search added for Exercise 22.23
+	 */
+
+	private TreeNode< T> findKey( T key ) {
+		if (root == null)
+			System.out.println("Tree Empty") ;
+
+		TreeNode< T> currentNode = root ;
+
+		while (currentNode != null)
+			if (currentNode.data.equals(key))
+				return currentNode ;
+			else if (currentNode.data.compareTo(key) > 0)
+				currentNode = currentNode.leftChild ;
+			else if (currentNode.data.compareTo(key) < 0)
+				currentNode = currentNode.rightChild ;
+		return currentNode ;
+	}
+
+	public T searchList( T value ) {
+		if (findKey(value) != null)
+			return value ;
+		else
+			return null ;
+	}
 }
 
-class TreeNode<T extends Comparable<T>> {
+class TreeNode<T extends Comparable< T>> {
 
-	TreeNode<T> leftChild ;
-	T data ;
-	TreeNode<T> rightChild ;
+	TreeNode< T>	leftChild ;
+	T				data ;
+	TreeNode< T>	rightChild ;
 
-
-	public TreeNode(T nodeData) {
+	public TreeNode ( T nodeData ) {
 		// TODO Auto-generated constructor stub
 		data = nodeData ;
 		leftChild = rightChild = null ;
@@ -104,23 +199,21 @@ class TreeNode<T extends Comparable<T>> {
 	/**
 	 * @param insertValue
 	 */
-	public void insert(T insertValue) {
+	public void insert( T insertValue ) {
 
-		if(insertValue.compareTo(data)< 0 )
+		if (insertValue.compareTo(data) < 0)
 		{
-			if(leftChild == null )
-				leftChild = new TreeNode<T>(insertValue);
-			else 
-				leftChild.insert(insertValue);
+			if (leftChild == null)
+				leftChild = new TreeNode< T>(insertValue) ;
+			else
+				leftChild.insert(insertValue) ;
 		}
 
-		else if(insertValue.compareTo(data)> 0 )
-		{
-			if ( rightChild == null )
-				rightChild = new TreeNode<T>(insertValue);
+		else if (insertValue.compareTo(data) > 0)
+			if (rightChild == null)
+				rightChild = new TreeNode< T>(insertValue) ;
 
-			else 
-				rightChild.insert(insertValue);
-		}
+			else
+				rightChild.insert(insertValue) ;
 	}
 }
